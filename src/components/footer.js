2abbf0a2e2aa9e2a9c158164861010529aca3099
abhaydee <abhaygirish1997@gmail.com>
2024-08-14 15:31:43 -0400

    changes to creds

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/abhaydee">
-          <div>Designed &amp; Built by Abhay Deshpande</div>
+          <div>Designed by Brittany Chiang &amp; Built by Abhay Deshpande</div>
 
           {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
